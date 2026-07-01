9679f3f1b7256dd6357c9907aaf250fccb3f41c4
David Leimbach <leimy2k@icloud.com>
2026-07-01 19:34:42 +0000

    Use libbio. Don't reinvent the wheel

--- util.c
+++ util.c
@@ -1,40 +1,30 @@
 #include <u.h>
 #include <libc.h>
+#include <bio.h>
 #include "util.h"
 
 /*
  * Slurp an entire file descriptor into a NUL-terminated malloced
  * buffer.  Shared by mkcoast and mkstations, both of which parse
  * their whole (potentially large) JSON input in one go.
+ *
+ * Brdstr(_, '\0', 0) does the work: if it hits end of file before
+ * finding the delimiter byte, it treats that the same as finding
+ * it, so it returns everything read so far as one malloced,
+ * NUL-terminated buffer.  Since our JSON input never contains an
+ * embedded NUL, asking for delimiter '\0' amounts to "read to EOF".
  */
 char *
 readall(int fd)
 {
-	char *buf;
-	long n, sz, tot;
+	Biobuf b;
+	char *s;
 
-	sz = 1<<20;
-	buf = malloc(sz);
-	if(buf == nil)
-		sysfatal("malloc: %r");
-	tot = 0;
-	for(;;){
-		if(tot >= sz){
-			sz *= 2;
-			buf = realloc(buf, sz);
-			if(buf == nil)
-				sysfatal("realloc: %r");
-		}
-		n = read(fd, buf+tot, sz-tot);
-		if(n < 0)
-			sysfatal("read: %r");
-		if(n == 0)
-			break;
-		tot += n;
-	}
-	buf = realloc(buf, tot+1);
-	if(buf == nil)
-		sysfatal("realloc: %r");
-	buf[tot] = 0;
-	return buf;
+	if(Binit(&b, fd, OREAD) < 0)
+		sysfatal("Binit: %r");
+	s = Brdstr(&b, '\0', 0);
+	Bterm(&b);
+	if(s == nil)
+		s = strdup("");	/* empty input */
+	return s;
 }
